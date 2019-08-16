7bfbef3c8952651ca9a96705b72d0b430f6cf9d2
root <root@msk-inventory.yamalgazprom.local>
2019-08-16 22:25:51 +0500

    added uis, webattic, squid

---
+++ manifests/ius.pp
@@ -0,0 +1,18 @@
+class repos::ius {
+	case $::operatingsystem {
+		CentOS: {
+			exec {"install_ius_repo":
+				command => "rpm -i http://centos${::operatingsystemmajrelease}.iuscommunity.org/ius-release.rpm",
+				unless => "rpm -qa | grep ius-release",
+				path => '/bin:/sbin:/usr/bin:/usr/sbin',
+			} #~>
+			#exec {"install_ius_key":
+			#	command => 'rpm --import http://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs',
+			#	path => '/bin:/sbin:/usr/bin:/usr/sbin',
+			#	refreshonly => true,
+			#}
+		}
+	}
+}
+
+
