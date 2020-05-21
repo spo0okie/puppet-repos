4ca723828a543c6db018b7cdb66a35c658d5e91d
Alexander Reviakin <reviakin@gmail.com>
2020-05-21 11:03:50 +0500

    Update zabbix.pp

--- manifests/zabbix.pp
+++ manifests/zabbix.pp
@@ -3,7 +3,7 @@ class repos::zabbix {
 		'CentOS': {
 			exec {"install_zabbix_repo":
 				command => "yum install https://repo.zabbix.com/zabbix/4.0/rhel/${::operatingsystemmajrelease}/x86_64/zabbix-release-4.0-1.el${::operatingsystemmajrelease}.noarch.rpm -y",
-				unless => "rpm -qa | grep zabbix-release-4.0",
+				unless => "rpm -qa | grep zabbix-release-4",
 				path => '/bin:/sbin:/usr/bin:/usr/sbin',
 			} 
 		}
