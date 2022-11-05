caed773f16b090f61a4cd743869c7a6b88186594
root <root@puppet.azimuth.holding.local>
2022-11-05 10:10:00 +0300

    remi repo only centos mode set

--- manifests/remi.pp
+++ manifests/remi.pp
@@ -1,13 +1,17 @@
 class repos::remi {
-	include repos
-	exec { 'install_remi_repo':
-		command => "rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-${::operatingsystemmajrelease}.rpm",
-		path => '/bin:/sbin:/usr/bin:/usr/sbin',
-		unless => 'ls -1 /etc/yum.repos.d | grep remi'
-	} ->
-	exec { 'install_remi_key':
-		command => "rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi",
-		path => '/bin:/sbin:/usr/bin:/usr/sbin',
-		refreshonly => true
+	case $::operatingsystem {
+		'CentOS': {
+			include repos
+			exec { 'install_remi_repo':
+					command => "rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-${::operatingsystemmajrelease}.rpm",
+					path => '/bin:/sbin:/usr/bin:/usr/sbin',
+					unless => 'ls -1 /etc/yum.repos.d | grep remi'
+				} ->
+				exec { 'install_remi_key':
+					command => "rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi",
+					path => '/bin:/sbin:/usr/bin:/usr/sbin',
+					refreshonly => true
+				}
+		}
 	}
 }
