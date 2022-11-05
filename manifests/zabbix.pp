64a72b88a2decbddaa1a37e7b0fc9e31c3f74685
root <root@puppet.azimuth.holding.local>
2022-11-05 10:14:46 +0300

    zabbix repo update to 6.2

--- manifests/zabbix.pp
+++ manifests/zabbix.pp
@@ -1,36 +1,26 @@
 class repos::zabbix {
-	case $::operatingsystem {
-		'CentOS': {
-			exec {"install_zabbix_repo":
-				command => "yum install https://repo.zabbix.com/zabbix/4.4/rhel/${::operatingsystemmajrelease}/x86_64/zabbix-release-4.4-1.el${::operatingsystemmajrelease}.noarch.rpm -y",
-				unless => "rpm -qa | grep zabbix-release-4.4",
-				path => '/bin:/sbin:/usr/bin:/usr/sbin',
-			} 
-		}
 
+	case $::operatingsystem {
 #Debian и Ubuntu согласно инструкции:
 #https://www.zabbix.com/documentation/4.4/ru/manual/installation/install_from_packages/debian_ubuntu
 		'Debian': {
 			case $::operatingsystemmajrelease {
-				'10': {
-					$packagename='zabbix-release_4.4-1+buster_all'
+				'11': {
+					$packagename='zabbix-release_6.2-1+debian11_all'
+				}
+				'10':  {
+					$packagename='zabbix-release_6.2-1+debian10_all'
 				}
 				'9':  {
-					$packagename='zabbix-release_4.4-1+stretch_all'
-				}
-				'8':  {
-					$packagename='zabbix-release_4.4-1+jessie_all'
+					$packagename='zabbix-release_6.2-1+debian9_all'
 				}
 			} 
-			exec {"download_zabbix_repo":
-				command => "wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/$packagename.deb",
-				unless => "test -f /tmp/$packagename.deb",
-				path => '/bin:/sbin:/usr/bin:/usr/sbin',
-				cwd => '/tmp',
-			} ->
+			file {"/tmp/$packagename.deb":
+				source => "puppet:///modules/repos/zabbix/$packagename.deb",
+			} ~>
 			exec {"install_zabbix_repo":
 				command => "dpkg -i /tmp/$packagename.deb && apt update",
-				unless => "dpkg -l | grep zabbix-release | grep 4.4",
+				unless => "dpkg -l | grep zabbix-release | grep 6.2",
 				path => '/bin:/sbin:/usr/bin:/usr/sbin',
 				cwd => '/tmp',
 			}
@@ -40,28 +30,28 @@ class repos::zabbix {
 
 		'Ubuntu': {
 			case $::operatingsystemmajrelease {
+				'22.04': {
+					$packagename='zabbix-release_6.2-1+ubuntu22.04_all'
+				}
 				'20.04': {
-					$packagename='zabbix-release_4.4-1+focal_all'
+					$packagename='zabbix-release_6.2-1+ubuntu20.04_all'
 				}
 				'18.04':  {
-					$packagename='zabbix-release_4.4-1+bionic_all'
+					$packagename='zabbix-release_6.2-1+ubuntu18.04_all'
 				}
 				'16.04':  {
-					$packagename='zabbix-release_4.4-1+xenial_all'
+					$packagename='zabbix-release_6.2-1+ubuntu16.04_all'
 				}
 				'14.04':  {
-					$packagename='zabbix-release_4.4-1+trusty_all'
+					$packagename='zabbix-release_6.2-1+ubuntu14.04_all'
 				}
 			} 
-			exec {"download_zabbix_repo":
-				command => "wget https://repo.zabbix.com/zabbix/4.4/ubuntu/pool/main/z/zabbix-release/$packagename.deb",
-				unless => "test -f /tmp/$packagename.deb",
-				path => '/bin:/sbin:/usr/bin:/usr/sbin',
-				cwd => '/tmp',
-			} ->
+			file {"/tmp/$packagename.deb":
+				source => "puppet:///modules/repos/zabbix/$packagename.deb",
+			} ~>
 			exec {"install_zabbix_repo":
 				command => "dpkg -i /tmp/$packagename.deb && apt update",
-				unless => "dpkg -l | grep zabbix-release | grep 4.4",
+				unless => "dpkg -l | grep zabbix-release | grep 6.2",
 				path => '/bin:/sbin:/usr/bin:/usr/sbin',
 				cwd => '/tmp',
 			}
